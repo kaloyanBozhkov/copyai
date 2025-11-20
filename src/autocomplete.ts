f23da1940daa18b8c2fd38d86bb22f6fd1d6572c
Kaloyan Bozhkov <your-email@example.com>
2025-11-21 05:59:44 +0700

    .

--- src/autocomplete.ts
+++ src/autocomplete.ts
@@ -2,25 +2,25 @@ import { allCategoryCommandsNameHashTable } from "./templateCommands";
 
 export const allCommandKeys = Object.keys(allCategoryCommandsNameHashTable);
 export const getClosestCommandKey = (input: string) => {
-  const key = allCommandKeys.find((key) => key.startsWith(input) && input !== key) ?? "";
+  const key = allCommandKeys.find((key) => key.startsWith(input)) ?? "";
+  const isFullmatch = key === input;
   if (!key) {
     return {
       key: "",
-      args: [],
+      args: isFullmatch ? getArgs(input) : [],
     };
   }
 
-  const argsCount = allCategoryCommandsNameHashTable[key].messageRecipe
-    .join("")
-    .split(/^\$[0-9]/).length;
-  const args = Array.from(
-    { length: argsCount },
-    (_, index) => `arg${index + 1}`
-  );
-  return { key, args };
+  return { key, args: getArgs(key) };
 };
 export type AutocompleteResult = {
   key: string;
   args: string[];
 };
 
+const getArgs = (key: string) => {
+  const argsCount = allCategoryCommandsNameHashTable[key].messageRecipe
+    .join("")
+    .split(/^\$[0-9]/).length;
+  return Array.from({ length: argsCount }, (_, index) => `arg${index + 1}`);
+};
