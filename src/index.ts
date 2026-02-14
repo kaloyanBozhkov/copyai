2707bf0fc5cc03c7de01c859032a8fa1df9205db
Kaloyan Bozhkov <kaloyan@bozhkov.com>
2026-02-14 20:53:00 +0200

    .

--- src/index.ts
+++ src/index.ts
@@ -6,8 +6,4 @@ import { join } from "path";
 import * as dotenv from "dotenv";
 dotenv.config({ path: join(__dirname, "..", ".env"), override: true });
 
-// load custom keys added via grimoire and override .env
-import { applyApiKeysToEnv } from "./kitchen/grimoireSettings";
-applyApiKeysToEnv();
-
 import "./app";
